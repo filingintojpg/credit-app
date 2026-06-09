e0d52e2850e0cc028739f89564b30f623e4346df
Ivan Maltsev <filingintojpg@gmail.com>
2026-06-10 00:50:33 +0300

    chore: Init project structure

---
+++ decision-service/src/test/java/com/practice/decision_service/DecisionServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practice.decision_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DecisionServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
