ba2436462194b61b609f8ce4fc173e1b7bfd0060
Ivan Maltsev <filingintojpg@gmail.com>
2026-06-20 02:06:02 +0300

    refactor: Move application validation from delegate to Camunda business-logic

--- decision-service/src/main/java/com/practice/decision_service/delegate/LoadApplicationDataDelegate.java
+++ decision-service/src/main/java/com/practice/decision_service/delegate/LoadApplicationDataDelegate.java
@@ -2,18 +2,18 @@ package com.practice.decision_service.delegate;
 
 import com.practice.decision_service.repository.ApplicationRepository;
 import org.camunda.bpm.engine.delegate.DelegateExecution;
+import org.camunda.bpm.engine.delegate.JavaDelegate;
 import org.springframework.stereotype.Component;
 
-import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.time.Period;
 
 @Component
-public class ValidateApplicationDelegate {
+public class LoadApplicationDataDelegate {
 
     private final ApplicationRepository applicationRepository;
 
-    public ValidateApplicationDelegate(ApplicationRepository applicationRepository) {
+    public LoadApplicationDataDelegate(ApplicationRepository applicationRepository) {
         this.applicationRepository = applicationRepository;
     }
 
@@ -21,19 +21,10 @@ public class ValidateApplicationDelegate {
         Long applicationId = (Long) execution.getVariable("applicationId");
 
         var data = applicationRepository.findApplicationData(applicationId);
-
-        BigDecimal amount = data.amount();
-        Integer term = data.term();
         int age = Period.between(data.birthDate(), LocalDate.now()).getYears();
 
-        boolean approved =
-                amount.compareTo(BigDecimal.valueOf(1000)) >= 0 &&
-                        amount.compareTo(BigDecimal.valueOf(30000)) <= 0 &&
-                        term >= 3 &&
-                        term <= 30 &&
-                        age >= 18 &&
-                        age <= 60;
-
-        execution.setVariable("approved", approved);
+        execution.setVariable("amount", data.amount());
+        execution.setVariable("term", data.term());
+        execution.setVariable("age", age);
     }
 }
