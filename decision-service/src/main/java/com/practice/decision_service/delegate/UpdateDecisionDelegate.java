62198be92f1a7893c2e54d5e8b5f8e681e8d91e6
Ivan Maltsev <filingintojpg@gmail.com>
2026-06-23 19:33:41 +0300

    fix: Add @Transactional to UpdateDecisionDelegate

--- decision-service/src/main/java/com/practice/decision_service/delegate/UpdateDecisionDelegate.java
+++ decision-service/src/main/java/com/practice/decision_service/delegate/UpdateDecisionDelegate.java
@@ -4,8 +4,10 @@ import com.practice.common.model.enums.DecisionStatus;
 import com.practice.common.repository.DecisionRepository;
 import org.camunda.bpm.engine.delegate.DelegateExecution;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 
 @Component
+@Transactional
 public class UpdateDecisionDelegate {
 
     private final DecisionRepository decisionRepository;
