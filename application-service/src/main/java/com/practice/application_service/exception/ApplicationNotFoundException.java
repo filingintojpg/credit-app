ccfc5dcfb9ca5d018ca94bc17e416a7c5384a3f8
Ivan Maltsev <filingintojpg@gmail.com>
2026-06-26 02:08:13 +0300

    refactor: Change ApplicationNotFound to EntityNotFound exception

--- application-service/src/main/java/com/practice/application_service/exception/ApplicationNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package com.practice.application_service.exception;
-
-public class ApplicationNotFoundException extends RuntimeException {
-
-    public ApplicationNotFoundException(Long id) {
-        super("Application not found: " + id);
-    }
-}
